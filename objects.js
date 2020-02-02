6d6461b555fc29bf12d5fa00f4a7fde0dbdb2390
Ioana Medvesan <ioanamedvesan@yahoo.com>
2020-02-02 01:27:52 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ function updatePlaylist (playlist, artistName, songTitle) {
   return playlist;
 }
 
-function removeFromPlaylist(playlist,artistName) {
-  delete playlist.artistName;
+function removeFromPlaylist(playlist, artistName) {
+  delete artistName.songTitle;
   return playlist;
 }
